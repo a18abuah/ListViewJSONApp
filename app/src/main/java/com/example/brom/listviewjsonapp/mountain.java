dab91057e18b4cf15c76409aa32e67b879d8b79e
a18abuah <a18abuah@student.his.se>
2019-04-29 14:49:34 +0200

    ny class

---
+++ app/src/main/java/com/example/brom/listviewjsonapp/mountain.java
@@ -0,0 +1,14 @@
+package com.example.brom.listviewjsonapp;
+
+public class mountain {
+    private String name;
+    private String location;
+    private int Highet;
+     public mountain (String stringname, String stringlocation, int inthighet){
+         name=stringname;
+         location=stringlocation;
+         Highet=inthighet;
+     }
+     
+}
+
